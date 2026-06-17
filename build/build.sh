02329457bade622c704183d9e73253da46413f8c
marker <j@saro.me>
2026-06-17 22:42:26 +0900

    v4.2.0
    - Support security tokens for server access
    - Update error handling
    - Overall refactoring

--- build/build.sh
+++ build/build.sh
@@ -6,5 +6,5 @@ rustup target add $TARGET
 mkdir -p target/bin
 rm -rf target/bin/$TARGET
 cargo build --release --target $TARGET
-cp target/$TARGET/release/dat-cms target/bin/$TEARGET
+cp "target/$TARGET/release/dat-cms" "target/bin/$TARGET"
 echo "$PWD/target/bin/$TARGET"
